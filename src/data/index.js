7e7d9197154bbb733573de43a88b600eb1d806ad
Digibit Connected <97073851+digibitconnected@users.noreply.github.com>
2022-01-04 12:32:40 +0700

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,5 @@
-import logoLight from "@/images/logo.png";
-import logoDark from "@/images/logo2.png";
+import logoLight from "@/images/logo-pencils.svg";
+import logoDark from "@/images/logo-pencils.svg";
 
 export const LogoImage = {
   light: logoLight,
